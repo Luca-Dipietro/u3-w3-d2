cbdcea99f76c797e493b3a2822f13dc168824f42
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-05-07 17:33:07 +0200

    fixing

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -1,48 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Job from "./Job";
 import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getCompanyAction } from "../redux/actions";
 
 const CompanySearchResults = () => {
-  const [jobs, setJobs] = useState([]);
+  const jobs = useSelector((state) => state.company.content.data);
   const params = useParams();
-
-  const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    getJobs();
+    dispatch(getCompanyAction(params.company));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const getJobs = async () => {
-    try {
-      const response = await fetch(baseEndpoint + params.company);
-      if (response.ok) {
-        const { data } = await response.json();
-        setJobs(data);
-      } else {
-        alert("Error fetching results");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <Container>
       <Row>
-        <Col className="my-3">
-          <h1 className="display-4">
-            Job posting for: {params.company}{" "}
-            <span className="display-6">
-              <Link to="/favorites">View Favorites</Link>
-            </span>
-          </h1>
+        {jobs && (
+          <Col className="my-3">
+            <h1 className="display-4">
+              Job posting for: {params.company}{" "}
+              <span className="display-6">
+                <Link to="/">Home</Link>
+              </span>{" "}
+              <span className="display-6">
+                <Link to="/favorites">View Favorites</Link>
+              </span>
+            </h1>
 
-          {jobs.map((jobData) => (
-            <Job key={jobData._id} data={jobData} />
-          ))}
-        </Col>
+            {jobs.map((jobData) => (
+              <Job key={jobData._id} data={jobData} />
+            ))}
+          </Col>
+        )}
       </Row>
     </Container>
   );
